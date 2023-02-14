0c0c1a3b2146a3fcca42eeea266229be435a7570
Baheer Payab <payabaheer.edu@gmail.com>
2023-02-14 15:09:55 +0400

    CALCULATOR v1.0 COMPLETE.

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const allBtns = document.querySelectorAll("button");
 const prevTxtDisplay = document.getElementById("previousTxt");
 const crntTxtDisplay = document.getElementById("currentTxt");
 
-// ADD KEYBOARD FUNCTIONALITY
-// Can't divide by 0
-// Clear and delete functions - completed
-
 let currentNum = "";
 let previousNum = "0";
 let newNum = "";
@@ -14,7 +10,6 @@ let currentTxt = "";
 let previousTxt = "";
 let summaryTxt = "";
 let operated = false;
-let numbers = 0;
 
 allBtns.forEach((button) => {
     button.addEventListener('click', () => {
@@ -22,12 +17,10 @@ allBtns.forEach((button) => {
     })
 });
 
-
 document.addEventListener("keydown", (e) => {
     if (e.key == "." || (e.key >= 0 && e.key <= 9)) {
       e.preventDefault();
       document.querySelector(`button[value="${e.key}"`).click(); 
-      console.log(e.key);
     } else if (e.key == "Enter" || e.key == "=") {
         e.preventDefault();
         document.getElementById("equals").click();
@@ -51,23 +44,6 @@ document.addEventListener("keydown", (e) => {
       document.getElementById("multiply").click()
       }
 });
-
-/*function handleKeyboardInput(e) {
-    if (e.key >= 0 && e.key <= 9) btnChecker(e.)
-    if (e.key === '.') appendPoint()
-    if (e.key === '=' || e.key === 'Enter') evaluate()
-    if (e.key === 'Backspace') deleteNumber()
-    if (e.key === 'Escape') clear()
-    if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/')
-      setOperation(convertOperator(e.key))
-  }
-  
-  function convertOperator(keyboardOperator) {
-    if (keyboardOperator === '/') return '÷'
-    if (keyboardOperator === '*') return '×'
-    if (keyboardOperator === '-') return '−'
-    if (keyboardOperator === '+') return '+'
-  }*/
 
 function btnChecker(button) {
     let btnAtt = button.getAttribute("data-type")
@@ -97,7 +73,6 @@ function numberInput(button) {
         if (prevTxtDisplay.textContent.includes("×")) selectedOperator = "multiply";
     }
     updateDisplay();
-    console.log(currentNum);
     }
 }
 
@@ -107,24 +82,19 @@ function operation(button) {
     }
     else if (button.getAttribute("id") == "equals" && prevTxtDisplay.textContent.includes("=") == false && currentNum !== "") {
         prevTxtDisplay.textContent += " " + `${currentNum}`;
-        console.log(selectedOperator);
         operated = false;
         equals();
-        console.log("this runs");
     }
     else if (selectedOperator !== "" && currentNum == "") {
         selectedOperator = button.getAttribute("id");
         operated = false;
         updateDisplay();
-        console.log("nah this runs ")
     } 
     else if (selectedOperator !== "") {
         operated = false;
         equals();
-        console.log("this runs too fsr")
     }
     selectedOperator = button.getAttribute("id");
-    console.log(selectedOperator);
     nextNumber();
 }
 
@@ -139,71 +109,53 @@ function calcFunction(button) {
     if (button.getAttribute("id") == "delete") {
         currentNum = crntTxtDisplay.textContent;
         currentNum = currentNum.slice(0, -1);
-        console.log(currentNum);
         crntTxtDisplay.textContent = crntTxtDisplay.textContent.slice(0, -1);     
     }
 }
 
 // NUMBER STORING
 
-function storeNum(numValue) {
-    if (currentNum == "") {
-    currentNum = numValue;
-    }
-    else {
-        currentNum += numValue;
-    }
-}
-
 function nextNumber() {
     if (currentNum == "") {
        return;
-    } else {
+    } 
+     else {
     previousNum = currentNum;
     currentNum = "";
-    //console.log(previousNum + "dop");
     }
-    //console.log(numbers);
-    console.log(selectedOperator);
     updateDisplay();
 }
 
 // MATH FUNCTIONS
 
 function divide(a, b) {
-    if (b == "0") {
-        alert("Why would you divide by 0? Just why?");
-
+    if (b == "0" || b =="0.") {
+        alert("Are you trying to break me? Why would you divide by 0? Just why?");
     } else {
     let divided = Math.round((parseFloat(a) / parseFloat(b)) * 1000) / 1000;
     currentNum = divided.toString();
-    console.log(currentNum);
-    return a / b;
     }
 }
 
 function multiply(a, b) {
-    let multiplied = Math.round(parseFloat(a) * parseFloat(b) * 1000) / 1000;
+    let multiplied = Math.round((parseFloat(a) * parseFloat(b)) * 1000) / 1000;
     currentNum = multiplied.toString();
-    //console.log(currentNum);
-    return a * b;
 }
 
 function subtract(a, b) {
-    let sub = parseFloat(a) - parseFloat(b);
+    let sub = Math.round((parseFloat(a) - parseFloat(b)) * 1000) / 1000;
     currentNum = sub.toString();
-    //console.log(currentNum);
-    return a - b;
 }
 
 function add(a, b) {
-    let sum = parseFloat(a) + parseFloat(b);
+    let sum = Math.round((parseFloat(a) + parseFloat(b)) * 1000) / 1000;
     currentNum = sum.toString();
-    //console.log(currentNum);
-    return a + b;
 }
 
 function equals() {
+    if (currentNum == ".") {
+        currentNum = "0.0";
+    }
     if (selectedOperator == "add") {
         add(previousNum, currentNum);
         selectedOperator = "";
@@ -221,13 +173,15 @@ function equals() {
         selectedOperator = "";
         operated = true;
     }
-    console.log(selectedOperator);
     updateDisplay();
 }
 
 // DISPLAY TEXT
 
 function updateDisplay() {
+    if (currentNum == ".") {
+        currentNum = "0.";
+    }
     if (selectedOperator == "") {
         crntTxtDisplay.textContent = `${currentNum}`;
     }
@@ -248,23 +202,19 @@ function updateDisplay() {
         prevTxtDisplay.textContent = `${previousNum}`;
         prevTxtDisplay.textContent += " " + "×";
     } if (selectedOperator == "equals" && prevTxtDisplay.textContent.includes("=") && currentNum == "") {
-        console.log("false")
         return;
     } else if (selectedOperator == "equals" && currentNum !== "" && previousNum == "") {
         prevTxtDisplay.textContent += " " + "=";
         crntTxtDisplay.textContent = `${previousNum}`;
-        console.log("equals sign boo")
         selectedOperator = "";
     } else if (selectedOperator == "equals" && operated == true) {
         prevTxtDisplay.textContent += " " + "=";
         crntTxtDisplay.textContent = `${previousNum}`;
-        console.log("equals sign")
         selectedOperator = "";
         operated = false;
     } else if (selectedOperator == "equals") {
         crntTxtDisplay.textContent = `${currentNum}`;
     }
 
-    console.log(selectedOperator);
     
 }
